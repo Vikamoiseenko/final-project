943331b21b375faa03b418ab2e853d3c2777bb5c
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-06-21 16:41:44 -0700

    deleted setPost

--- handlers/deletePost.js
+++ handlers/deletePost.js
@@ -1,9 +1,21 @@
 var Post = require("../models/post");
 
 module.exports = function(req, reply) {
-    var posts = new Post();
-    posts.delete(function() {
-      data = posts.toJSON();
-        reply.redirect("/list");
-   });
+  var id = req.params.id;
+  var model = new Post({
+    id: id
+  });
+  model.set("id", id);
+  model.delete(function(err) {
+    var data;
+    if(err) {
+      console.log(err);
+    } else {
+      data = model.toJSON();
+    }
+    reply.view("delete", {
+      title: data.name,
+      library: model.toJSON()
+    });
+  }) ;
 };
